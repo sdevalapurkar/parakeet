53f5102e395b535e18e5b9e9f40960915ea08dda
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-12 22:18:27 -0700

    fix padding

--- app/src/components/NewGoalDialog.js
+++ app/src/components/NewGoalDialog.js
@@ -30,55 +30,59 @@ function NewGoalDialog(props) {
           Enter a new goal you wish to work towards achieving! Try to make sure it is something
           you can complete.
         </DialogContentText>
-        <TextField
-          className={classes.textFieldMargin}
-          type="text"
-          name="goal_name"
-          label="What's your goal?"
-          id="goal_name"
-          onChange={(e) => props.setGoalName(e.target.value)}
-          variant="outlined"
-          value={props.goalName}
-          fullWidth={true}
-          placeholder="Learn how to play G Major scale with slurs"
-        />
-        <TextField
-          className={classes.textFieldMargin}
-          type="number"
-          name="goal_times"
-          label="How many times do you need to practice it? Please enter a number."
-          id="goal_times"
-          onChange={(e) => props.setGoalTimes(e.target.value)}
-          variant="outlined"
-          value={props.goalTimes}
-          fullWidth={true}
-          placeholder="25"
-        />
-        <TextField
-          className={classes.textFieldMargin}
-          fullWidth
-          id="goal_start_date"
-          name="goal_start_date"
-          label="Start Date"
-          variant="outlined"
-          required={true}
-          value={props.goalStartDate}
-          type="date"
-          onChange={(e) => props.setGoalStartDate(e.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
-        <TextField
-          fullWidth
-          id="goal_end_date"
-          name="goal_end_date"
-          label="End Date"
-          variant="outlined"
-          required={true}
-          value={props.goalEndDate}
-          type="date"
-          onChange={(e) => props.setGoalEndDate(e.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
+        <Box pt={2}>
+          <TextField
+            className={classes.textFieldMargin}
+            type="text"
+            name="goal_name"
+            label="What's your goal?"
+            id="goal_name"
+            onChange={(e) => props.setGoalName(e.target.value)}
+            variant="outlined"
+            value={props.goalName}
+            fullWidth={true}
+            required={true}
+            placeholder="Learn how to play G Major scale with slurs"
+          />
+          <TextField
+            className={classes.textFieldMargin}
+            type="number"
+            name="goal_times"
+            label="How many times do you need to practice it? Please enter a number."
+            id="goal_times"
+            onChange={(e) => props.setGoalTimes(e.target.value)}
+            variant="outlined"
+            value={props.goalTimes}
+            fullWidth={true}
+            required={true}
+            placeholder="25"
+          />
+          <TextField
+            className={classes.textFieldMargin}
+            fullWidth
+            id="goal_start_date"
+            name="goal_start_date"
+            label="Start Date"
+            variant="outlined"
+            required={true}
+            value={props.goalStartDate}
+            type="date"
+            onChange={(e) => props.setGoalStartDate(e.target.value)}
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            fullWidth
+            id="goal_end_date"
+            name="goal_end_date"
+            label="End Date"
+            variant="outlined"
+            required={true}
+            value={props.goalEndDate}
+            type="date"
+            onChange={(e) => props.setGoalEndDate(e.target.value)}
+            InputLabelProps={{ shrink: true }}
+          />
+        </Box>
       </DialogContent>
       <Box display="flex" justifyContent="flex-end" pt={1} pr={3} pb={2}>
         <Button
